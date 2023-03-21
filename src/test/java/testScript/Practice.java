ff22e1a88885ee7da67bde19f310161949f249af
RR <RR@DESKTOP-N4OFCKJ>
2023-03-22 01:25:35 +0530

    Commit in Weekly Branch 1

---
+++ src/test/java/testScript/Practice.java
@@ -0,0 +1,11 @@
+package testScript;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+
+	@Test
+	public void demo() {
+		System.out.println("First Commit to GitHub in Ad.Sel Session");
+	}
+}
