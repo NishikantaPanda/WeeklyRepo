f66ef95e0deff7293c50216a0300c3d4a87524e3
RR <RR@DESKTOP-N4OFCKJ>
2023-03-22 01:34:32 +0530

    Second Commit in Weekly Branch

--- src/test/java/testScript/Practice.java
+++ src/test/java/testScript/Practice.java
@@ -1,11 +1,20 @@
 package testScript;
 
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Practice {
 
 	@Test
 	public void demo() {
 		System.out.println("First Commit to GitHub in Ad.Sel Session");
+	}
+	
+	@Test
+	public void demo2() {
+		SoftAssert sa = new SoftAssert();
+		System.out.println("Hello from ProfessoR");
+		sa.assertTrue(true);
+		sa.assertAll();
 	}
 }
